510c72b9f65a55be364f3ad29d6c504ff305ee07
Andrew Glover <ajglover@gmail.com>
2011-08-22 21:35:08 -0400

    changed from get to put

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+app.use(express.bodyParser());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.put('/', function(req, res) {
+  console.log(req.body.account);
+  res.send('Hello World!');
 });
 
 var port = process.env.PORT || 3000;
