8ed9ebb1b04a6bc7b9a753e7bf1aabe4cdd53c7c
Andrew Glover <ajglover@gmail.com>
2011-08-27 16:38:44 -0400

    fixed key

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 app.use(express.bodyParser());
 
 app.put('/', function(req, res) {
-  console.log(req.body.deal_descrption);
+  console.log(req.body.deal_description);
   res.send('Hello World!');
 });
 
